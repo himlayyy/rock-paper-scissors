be68da86c6f8cf6401bc8fa26b198a2453c94c47
himlayyy <alesandralelis@gmail.com>
2022-01-10 15:33:18 +0800

    Added UI

--- index.js
+++ index.js
@@ -1,44 +1,100 @@
-console.log("hello!");
+const choices = ["Rock", "Paper", "Scissors"]
 
-const choices = ["rock", "paper", "scissors"];
+function displayChoices(playerChoice, computerChoice) {
+    let playerMove = document.querySelector("#player-choice");
+    let computerMove = document.querySelector("#computer-choice");
 
-function computerPlay() {
-    let compChoice = choices[Math.floor(Math.random() * choices.length)];
-    return compChoice;
+    playerMove.src = `${playerChoice}-user.png`;
+    computerMove.src = `${computerChoice}-comp.png`;
+
+
 }
-function playRound(playerSelection, computerSelection) {
-    let msg = "";
-    if (playerSelection === computerSelection) {
-        msg = "It's a draw";
-    }
-    else if ((playerSelection === "paper") && (computerSelection === "scissors") ||
-        (playerSelection === "rock") && (computerSelection === "paper") ||
-        (playerSelection === "scissors") && (computerSelection === "rock")) {
-        msg = `You win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else if ((playerSelection === "paper") && (computerSelection === "rock") ||
-        (playerSelection === "rock") && (computerSelection === "scissors") ||
-        (playerSelection === "scissors") && (computerSelection === "paper")) {
-        msg = `You lose! ${playerSelection} lost to ${computerSelection}`;
-    }
-    else {
-        msg = `${playerSelection} is not a valid input`;
-    }
-    return msg;
-}
+function displayResult(message, winner) {
+    let messageContainer = document.querySelector(".result");
+    if (winner != null) {
+        if (winner === "comp") {
+            inc = document.querySelector("#computer-score");
+        }
+        else if (winner === "player") {
+            inc = document.querySelector("#player-score");
+        }
 
-function game(rounds) {
-    let i = 0;
-    while (i < rounds) {
-        console.log(`Round #${i+1}`);
-        const playerSelection = prompt("Your choice").toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(computerSelection);
-        console.log(playerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        i++;
+        let newScore = parseInt(inc.innerHTML) + 1;
+        inc.innerHTML = newScore;
+        if (newScore === 5) {
+            console.log(`${winner} won`);
+            gameWinner = document.createElement("p");
+            gameWinner.innerHTML = `Game over! ${winner.toUpperCase()} won`;
+            disableButtons();
+        }
+        messageContainer.innerHTML = message;        
+        messageContainer.append(gameWinner);
+    }
+    else{
+        messageContainer.innerHTML = message;
     }
 }
-const rounds = 5;
 
-game(rounds);
+function evaluateRound(playerChoice) {
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)].toLowerCase();
+    displayChoices(playerChoice, computerChoice);
+    let message = "";
+    let winner = "";
+    switch (playerChoice + computerChoice) {
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            message = "Tie";
+            winner = null;
+            break;
+        case "scissorspaper":
+        case "paperrock":
+        case "rockscissors":
+            winner = "player";
+            message = "You Win!";
+            break;
+        case "rockpaper":
+        case "scissorsrock":
+        case "paperscissors":
+            winner = "comp";
+            message = "You lose!"
+            break;
+    }
+    displayResult(message, winner);
+}
+
+function disableButtons() {
+    let buttons = document.querySelectorAll(".button");
+    buttons.forEach(button => {
+        button.remove();
+    }
+    )
+    replay()
+}
+function replay() {
+    let replayBtn = document.createElement("button");
+    replayBtn.innerHTML = "Replay";
+    replayBtn.setAttribute("class", "button");
+    let options = document.querySelector(".options");
+    options.appendChild(replayBtn);
+    replayBtn.addEventListener("click", () => {
+        location.reload(true);
+
+    })
+    
+}
+let makeBtns = choices.forEach(
+    (choice) => {
+        let options = document.querySelector(".options");
+        let btn = document.createElement("button");
+        btn.setAttribute("class", "button");
+        btn.id = choice.toLowerCase();
+        btn.innerText = choice;
+        options.appendChild(btn);
+        btn.addEventListener('click', function (e) {
+            evaluateRound(e.target.id);
+        });
+    }
+)
+makeBtns
+
